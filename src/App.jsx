d88365ec9f8015f90e2e24b788032f8a63f542e7
doyun5223 <syhyh2000@hanmail.net>
2023-11-20 17:41:19 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import JSConfetti from "js-confetti";
+import "./index.css";
+
+function App() {
+  const [wait, setWait] = useState(true);
+  const jsConfetti = new JSConfetti();
+
+  const handleConfetti = async () => {
+    setWait(false);
+    await jsConfetti
+      .addConfetti({
+        emojis: ["🤩", "🎟️", "💚", "🎃", "🎄"],
+        emojiSize: 80,
+        comfettiNumber: 55,
+      })
+      .then(() => setWait(true));
+  };
+
+  return (
+    <div id="content">
+      <button disabled={!wait} onClick={handleConfetti}>
+        {wait ? "Wow" : ""}
+      </button>
+    </div>
+  );
+}
+
+export default App;
