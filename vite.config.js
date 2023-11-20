458f7fc278cf1130424a85e8e27f9c1f97663b98
doyun5223 <syhyh2000@hanmail.net>
2023-11-20 17:55:33 +0900

    init

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/doyun5223/",
+  base: "/https://doyun5223.github.io/dyblog//",
   plugins: [react()],
 });
